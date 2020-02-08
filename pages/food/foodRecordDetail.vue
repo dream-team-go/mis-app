dc47cdd093467a627e8dd955edf9eb9f4b70d420
杨洪娜 <2454970204@qq.com>
2020-02-08 14:33:54 +0800

    style

--- pages/food/foodRecordDetail.vue
+++ pages/food/foodRecordDetail.vue
@@ -96,8 +96,12 @@
 			</view>
 		</view>
 		
-		<view class="padding flex flex-direction" v-if="info.status == 0">
+		<!-- <view class="padding flex flex-direction" v-if="info.status == 0">
 			<button class="cu-btn bg-gradual-orange margin-tb-sm lg" @click="cancleBook">取消预定</button>
+		</view> -->
+		<view class="bottom-btns-seat" v-if="info.status == 0"></view>
+		<view class="bottom-cancel-btn"  @click="cancleBook">
+			取消预定
 		</view>
 	</view>
 </template>
@@ -231,6 +235,7 @@
 	}
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "style/mystyle.scss";
 
 </style>
